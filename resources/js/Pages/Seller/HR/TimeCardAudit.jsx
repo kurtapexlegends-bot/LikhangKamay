a5bf2782a1e68d557f7f1ef2fb3dd5584a1b81bd
Kurt Stanley Talastas <kurtapexlegends@gmail.com>
2026-08-14 20:42:09 +0800

    fix(hr): import useMemo in TimeCardAudit

--- resources/js/Pages/Seller/HR/TimeCardAudit.jsx
+++ resources/js/Pages/Seller/HR/TimeCardAudit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import { Head, Link, router } from '@inertiajs/react';
 import SellerHeader from '@/Layouts/SellerHeader';
 import SellerWorkspaceLayout, { useSellerWorkspaceShell } from '@/Layouts/SellerWorkspaceLayout';
